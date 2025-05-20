1b99c72539cb8c3a770451a93f91d6bc01c01509
Geronimo Morales <geronimorales@gmail.com>
2025-05-20 00:03:23 -0300

    minor change in main

--- src/assistant/main.py
+++ src/assistant/main.py
@@ -4,21 +4,26 @@ from fastapi.middleware.cors import CORSMiddleware
 from assistant.config.app import config
 from assistant.api.routes import api_router
 
-app = FastAPI(
-    title=config.get("api.title"),
-    version=config.get("api.version"),
-    debug=config.get("app.debug"),
-)
-
-# Configure CORS
-if config.get("app.env") == "local":
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=config.get("cors.origins"),
-        allow_credentials=True,
-        allow_methods=config.get("cors.methods"),
-        allow_headers=config.get("cors.headers"),
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title=config.get("api.title"),
+        version=config.get("api.version"),
+        debug=config.get("app.debug"),
     )
 
-# Include routers
-app.include_router(api_router, prefix=config.get("api.prefix"))
+    # Configure CORS
+    if config.get("app.env") == "local":
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=config.get("cors.origins"),
+            allow_credentials=True,
+            allow_methods=config.get("cors.methods"),
+            allow_headers=config.get("cors.headers"),
+        )
+
+    # Include routers
+    app.include_router(api_router, prefix=config.get("api.prefix"))
+
+    return app
+
+app = create_app()
